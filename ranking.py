c104da59e4154b552b4dc244f8904f39f1c12439
hiroakiitou <pigaru.gooood.mar19@gmail.com>
2021-01-02 12:57:40 +0900

    出力ファイル名変更

--- ranking.py
+++ ranking.py
@@ -2,6 +2,7 @@ import requests
 import urllib
 import numpy as np
 import pandas as pd
+import datetime
 
 def get_api(url):
     result = requests.get(url)
@@ -24,7 +25,8 @@ def make_csv(responses):
     items_df = items_df.reindex(columns=['rank', 'itemName', 'catchcopy', 'itemCode', 'itemPrice', 'itemCaption', 'itemUrl', 'imageFlag', 'availability', 'creditCardFlag', 'shipOverseasFlag', 'shipOverseasArea', 'asurakuFlag', 'asurakuArea', 'shopName', 'shopCode', 'shopUrl'])
     items_df.columns = ['ランキング順位', '商品名', 'キャッチコピー', '商品コード', '商品価格', '商品説明文', '商品URL', '商品画像有無', '販売可否', 'クレジットカード利用可否', '海外配送可否', '海外配送対象地域', 'あす楽可否', 'あす楽配送対象地域', '店舗名', '店舗コード', '店舗URL']
     items_df.index = np.arange(1, len(items_df)+1)
-    items_df.to_csv('./rakuten_ranking.csv', encoding='utf_8_sig')
+    now = datetime.datetime.now().strftime('%Y%m%d')
+    items_df.to_csv("./rakuten_ranking_{}.csv".format(now), encoding='utf_8_sig')
 
 def main():
     age= "20"
